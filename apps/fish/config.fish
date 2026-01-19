427e4f43a90801230d02431288d3b9654f34ca5d
Jose Rabasco <rabasco@gmail.com>
2026-01-19 08:55:31 +0100

    Update

--- apps/fish/config.fish
+++ apps/fish/config.fish
@@ -3,12 +3,19 @@ if status is-interactive
 end
 
 # Enable FZF
-fzf --fish | source
+#fzf --fish | source
 
 # Enable starship (prompt configurator)
 starship init fish | source
 
 # aliases
+
+## Global
+alias icat="kitten icat"
+alias s="kitten ssh"
+alias d="kitten diff"
+
+# Work
 alias wiremock="~/code/ios-app/wiremock/start.sh"
 alias format="~/code/ios-app/BuildTools/CheckSwiftformat/run-swiftformat.sh ../.. --config ../../config.swiftformat"
 alias mocks="~/code/ios-app/Other/scripts/run-automockable.sh"
